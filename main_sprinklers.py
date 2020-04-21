eb855e68daf50a4c305a09a129cb0d7ed96679e7
Shan <vigneswaran.shanmugathas@nokia.com>
2020-04-21 22:51:31 +0200

    feat(main_sprinklers.py): print tft error, change bg color power=1 green, power=0 red

--- main_sprinklers.py
+++ main_sprinklers.py
@@ -12,7 +12,7 @@ NODE_TYPE = node_type.SPRINKLER
 SPRINKLER_TAG = 'orchid'
 PK_API_GATEWAY_HOST = '10.3.141.1'
 PK_API_GATEWAY_PORT = 8001
-POWER_COLOR = TFT.RED
+POWER_COLOR = TFT.GREEN
 
 spi = SPI(
     2,
@@ -28,7 +28,7 @@ tft.initb2()
 tft.rgb(True)
 tft.fill(TFT.BLACK)
 tft.fillrect((0, 0), (128, 50), TFT.WHITE)
-tft.fillrect((0, 50), (128, 160), POWER_COLOR)
+tft.fillrect((0, 50), (128, 160), TFT.RED)
 tft.text((2, 2), "Wifi: " + SSID, TFT.BLACK, sysfont, 1.1, nowrap=False)
 tft.text((2, 10), "Api Gateway:", TFT.BLACK, sysfont, 1.1, nowrap=False)
 tft.text((2, 20), PK_API_GATEWAY_HOST + ":" + str(PK_API_GATEWAY_PORT), TFT.BLACK, sysfont, 1.1, nowrap=False)
@@ -41,10 +41,17 @@ pk = PlantKeeper(
     port=PK_API_GATEWAY_PORT
 )
 pk.set_node_type(node_type=NODE_TYPE)
+last_power = False
 
 if __name__ == '__main__':
     while True:
         try:
+            if last_power:
+                POWER_COLOR = TFT.GREEN
+            else:
+                POWER_COLOR = TFT.RED
+
+            tft.fillrect((0, 50), (128, 160), POWER_COLOR)
             sensor = random.randint(10, 30)
             pk.post({"soil_humidity": sensor, 'tag': SPRINKLER_TAG})
 
@@ -54,12 +61,22 @@ if __name__ == '__main__':
             tft.fillrect((40, 70), (30, 10), POWER_COLOR)
             tft.text((2, 70), "Power: " + str(pk.power), TFT.BLACK, sysfont, 1.1, nowrap=False)
 
-            if pk.power == 1:
-                POWER_COLOR = TFT.GREEN
-            else:
-                POWER_COLOR == TFT.RED
-
+            last_power = pk.power
             gc.collect()
             time.sleep(0.1)
         except:
-            pass
+            last_power = 0
+            tft.fillrect((95, 60), (30, 10), POWER_COLOR)
+            tft.text((2, 60), "ERROR ", TFT.BLACK, sysfont, 2, nowrap=False)
+            tft.text((2, 80), "POWER=0 ", TFT.BLACK, sysfont, 2, nowrap=False)
+            tft.text(
+                (2, 100),
+                "Reason:  "
+                " can not communicate with  API Gateway, "
+                "is   the  server  up   and running ?",
+                TFT.BLACK,
+                sysfont,
+                1.2,
+                nowrap=False
+            )
+            time.sleep(2)
