8e7d47764c39c460d0baef88e07381bba46a86bc
shan <shanmugathas.vigneswaran@outlook.fr>
2020-05-01 23:52:48 +0200

    feat(main_sprinklers.py): upgrade with fitting analog signal to 0-100% soil moisture

--- main_sprinklers.py
+++ main_sprinklers.py
@@ -1,18 +1,42 @@
+"""
+
+Plant keeper Sprinkler ESP32 firmware
+Use:
+    - Pin 34 for Capacitive soil moisture sensor
+    - Pin 26 for sprinkler valve
+    - TFT Screen ST7735 Pins: 12,13,14,16, 17, 18
+
+Author : Shanmugathas Vigneswaran
+mail : shangmuathas.vigneswaran@outlook.fr
+
+"""
 import gc
-import random
+import time
 from boot import SSID
 from sysfont import sysfont
 from ST7735 import TFT
-from machine import SPI, Pin
+from machine import SPI, Pin, ADC
+from utils import fit
 from pk import PlantKeeper
 import node_type
-import time
+
 
 NODE_TYPE = node_type.SPRINKLER
 SPRINKLER_TAG = 'orchid'
 PK_API_GATEWAY_HOST = '10.3.141.1'
 PK_API_GATEWAY_PORT = 8001
 POWER_COLOR = TFT.GREEN
+
+SOIL_MOISTURE = ADC(Pin(34))
+SOIL_MOISTURE.atten(ADC.ATTN_11DB)
+SOIL_MOISTURE_FIT = fit(
+    # MAP Analog read min/max
+    [2300, 1110],
+    # to 0% to 100%
+    [0, 100]
+)
+
+RELAY = Pin(26, Pin.OUT)
 
 spi = SPI(
     2,
@@ -46,24 +70,32 @@ last_power = False
 if __name__ == '__main__':
     while True:
         try:
+            sensor = SOIL_MOISTURE_FIT(SOIL_MOISTURE.read())
+            pk.post({"soil_humidity": sensor, 'tag': SPRINKLER_TAG})
+
             if last_power:
                 POWER_COLOR = TFT.GREEN
             else:
                 POWER_COLOR = TFT.RED
 
             tft.fillrect((0, 50), (128, 160), POWER_COLOR)
-            sensor = random.randint(10, 30)
-            pk.post({"soil_humidity": sensor, 'tag': SPRINKLER_TAG})
-
+            tft.text((2, 50), "Tag: " + SPRINKLER_TAG, TFT.BLACK, sysfont, 1.1, nowrap=False)
             tft.fillrect((95, 60), (30, 10), POWER_COLOR)
             tft.text((2, 60), "Soil humidity: " + str(sensor), TFT.BLACK, sysfont, 1.1, nowrap=False)
-
             tft.fillrect((40, 70), (30, 10), POWER_COLOR)
             tft.text((2, 70), "Power: " + str(pk.power), TFT.BLACK, sysfont, 1.1, nowrap=False)
 
+            if not last_power:
+                last_power = pk.power
+                RELAY.value(pk.power)
+            else:
+                if pk.power != last_power:
+                    RELAY.value(pk.power)
+
             last_power = pk.power
             gc.collect()
-            time.sleep(0.1)
+            time.sleep(0.5)
+
         except:
             last_power = 0
             tft.fillrect((95, 60), (30, 10), POWER_COLOR)
