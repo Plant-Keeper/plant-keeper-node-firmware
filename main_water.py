715b4f9f7f76c0f9e7b7b080d4161102b687654e
Shanmugathas Vigneswaran <vigneswaran.shanmugathas@nokia.com>
2020-08-20 23:22:54 +0200

    feat(main_water.py): use thread + mqtt

--- main_water.py
+++ main_water.py
@@ -1,182 +1,178 @@
 """
+Node type Water main.py file.
+Development version, for production use minified main.py
+in water-firmware/main.py
 
-Plant keeper Water ESP32 firmware
-Use:
-    - Pin 21 for HCSR04 trigger, 22 for echo, use +5V not +3.3V
-    - Pin 26 for Pump activation
-    - TFT Screen ST7735 Pins: 4, 2, 23, 18, 15
-        tested with supplier = Az Delevery
-
-Author : Shanmugathas Vigneswaran
-mail : shangmuathas.vigneswaran@outlook.fr
-
+Author: Shanmugathas Vigneswaran
+email: vigneswaran.shanmugathas@outlook.fr
 """
-__version__ = 'wip-no-release'
 
 import gc
+import ujson
 import time
-from boot import SSID
-from sysfont import sysfont
+import _thread
 from ST7735 import TFT
-from hcsr04 import HCSR04
-from machine import SPI, Pin, ADC
-from utils import fit
-from pk import PlantKeeper
-import node_type
+from sysfont import sysfont
+from umqtt.robust import MQTTClient
 
-# Node and Api Gateway setting
-NODE_TYPE = node_type.WATER
-PK_API_GATEWAY_HOST = '192.168.0.5'
-PK_API_GATEWAY_PORT = 31801
-# Soil humidity sensors setting
-water_level_sensor = HCSR04(trigger_pin=21, echo_pin=22, echo_timeout_us=10000)
-water_level_fitter = fit(
-    # Map analog read min/max
-    [118, 34],
-    # to 0% to 100%
-    [0, 100]
-)
+from settings import tft
+from settings import MQTT_SERVER
+from settings import MQTT_PORT
+from settings import WIFI_SSID
+from settings import __water_firmware_version__
 
-# https://en.wikipedia.org/wiki/PH
-ph_sensor = ADC(Pin(33))
-ph_sensor.atten(ADC.ATTN_11DB)
-ph_fitter = fit(
-    # Map analog read min/max
-    # don't change this values for ADC
-    [0, 4095],
-    # acidity level
-    [0, 14]
-)
+from water_io import read_sensors
+from water_io import water_pump_relay
+from water_io import nutrient_pump_relay
+from water_io import ph_downer_pump_relay
+from water_io import probe_cleaning_relay
 
-# https://en.wikipedia.org/wiki/Conductivity_(electrolytic)
-ec_sensor = ADC(Pin(34))
-ec_sensor.atten(ADC.ATTN_11DB)
-ec_fitter = fit(
-    # Map analog read min/max
-    # don't change this values for ADC
-    [0, 4095],
-    # depends on used sensor
-    # target default range [5 ; 50]
-    # units mS/m
-    [5, 50]
-)
+gc.enable()
 
-# https://en.wikipedia.org/wiki/Reduction_potential
-orp_sensor = ADC(Pin(35))
-orp_sensor.atten(ADC.ATTN_11DB)
-orp_fitter = fit(
-    # Map analog read min/max
-    # don't change this values for ADC
-    [0, 4095],
-    # depend on used sensor
-    # target default range [-400 ; 400]
-    # units milliVolt
-    [-400, 400]
-)
+NODE_TYPE = "water"
+_SENSOR_TOPIC = NODE_TYPE + "/sensor"
+_CONTROLLER_TOPIC = NODE_TYPE + "/controller"
+_TFT = tft
 
-# Relay for valve power on / power off
-PUMP_RELAY = Pin(19, Pin.OUT)
+registered = False
+flow_dict = {
+    "current": {
+        "water_pump_signal": False,
+        "nutrient_pump_signal": False,
+        "ph_downer_pump_signal": False,
+        "probe_cleaning_signal": False
+    },
+    "last": {
+        "water_pump_signal": False,
+        "nutrient_pump_signal": False,
+        "ph_downer_pump_signal": False,
+        "probe_cleaning_signal": False
+    },
+    "sensors": {},
+    # if controller callback timeout
+    # active soft fuse
+    # soft will put all actuator security state
+    # off or on : depends on actuator
+    "updated_at": time.time(),
+    'soft_fuse': False
+}
 
-SPI = SPI(
-    2, baudrate=20000000,
-    polarity=0, phase=0,
-    sck=Pin(18), mosi=Pin(23), miso=Pin(12)
-)
-POWER_COLOR = TFT.GREEN
 
-# Print Node information / Static section
-tft = TFT(SPI, 2, 4, 15)
-tft.initb2()
-tft.rgb(True)
-tft.fill(TFT.BLACK)
-tft.fillrect((0, 0), (128, 50), TFT.WHITE)
-tft.fillrect((0, 50), (128, 160), TFT.RED)
-tft.text((2, 2), "Wifi: " + SSID, TFT.BLACK, sysfont, 1.1, nowrap=False)
-tft.text((2, 10), "Api Gateway:", TFT.BLACK, sysfont, 1.1, nowrap=False)
-tft.text((2, 20), PK_API_GATEWAY_HOST + ":" + str(PK_API_GATEWAY_PORT), TFT.BLACK, sysfont, 1.1, nowrap=False)
-tft.text((2, 30), "NodeType:", TFT.BLACK, sysfont, 1.1, nowrap=False)
-tft.text((2, 40), NODE_TYPE, TFT.BLACK, sysfont, 1.1, nowrap=False)
+def subscribe_controller():
+    global flow_dict
 
-# Create Plant Keeper Client
-pk = PlantKeeper(
-    host=PK_API_GATEWAY_HOST,
-    port=PK_API_GATEWAY_PORT
-)
-pk.set_node_type(node_type=NODE_TYPE)
+    def callback(topic, msg):
+        d = ujson.loads(msg)
+        flow_dict['current']['water_pump_signal'] = d['water_pump_signal']
+        flow_dict['current']['nutrient_pump_signal'] = d['nutrient_pump_signal']
+        flow_dict['current']['ph_downer_pump_signal'] = d['ph_downer_pump_signal']
+        flow_dict['current']['probe_cleaning_signal'] = d['probe_cleaning_signal']
+        flow_dict['updated_at'] = time.time()
+        water_pump_relay.value(d['water_pump_signal'])
+        nutrient_pump_relay.value(d['nutrient_pump_signal'])
+        ph_downer_pump_relay.value(d['ph_downer_pump_signal'])
+        probe_cleaning_relay.value(d['probe_cleaning_signal'])
 
-while not pk.is_gateway_up():
-    tft.fillrect((0, 50), (128, 160), TFT.RED)
-    tft.text((2, 60), "ERROR ", TFT.BLACK, sysfont, 2, nowrap=False)
-    tft.text((2, 80), "Can not reach Api-Gateway, is Api-Gateway up ?", TFT.BLACK, sysfont, 1.1, nowrap=False)
-    gc.collect()
-    time.sleep(2)
-
-last_power = False
-if __name__ == '__main__':
+    c = MQTTClient(
+        NODE_TYPE
+        + "_SUB",
+        MQTT_SERVER,
+        MQTT_PORT
+    )
+    c.set_callback(callback)
+    c.connect()
+    c.subscribe(_CONTROLLER_TOPIC)
     while True:
-        try:
-            raw_distance = water_level_sensor.distance_mm()
-            percent = int(water_level_fitter(raw_distance))
+        c.wait_msg()
 
-            ph_raw = ph_sensor.read()
-            ph = ph_fitter(ph_raw)
 
-            ec_raw = ec_sensor.read()
-            ec = ec_fitter(ec_raw)
+def publish_sensors():
+    global flow_dict
 
-            orp_raw = orp_sensor.read()
-            orp = orp_fitter(orp_raw)
+    c = MQTTClient(
+        NODE_TYPE
+        + "_PUB",
+        MQTT_SERVER,
+        MQTT_PORT
+    )
+    c.connect()
+    while True:
+        s = read_sensors()
+        flow_dict['sensors'] = s
+        c.publish(_SENSOR_TOPIC, s['influx_message'])
 
-            # Post to Api Gateway sensor value
-            pk.post({"level": percent})
-            # +----------------------------------------------------+
-            # | Builtin TFT screen processing (dynamic zone)       |
-            # +----------------------------------------------------+
-            # full back ground GREEN = Power ON / RED = Power OFF
-            if last_power:
-                POWER_COLOR = TFT.GREEN
-            else:
-                POWER_COLOR = TFT.RED
-            # Flush dynamic part of screen
-            tft.fillrect((0, 50), (128, 160), POWER_COLOR)
-            # Print relevant values for user
-            tft.text((2, 50), "Raw dist. (mm) : " + str(raw_distance), TFT.BLACK, sysfont, 1.1, nowrap=False)
-            tft.text((2, 60), "Water filled %: " + str(percent), TFT.BLACK, sysfont, 1.1, nowrap=False)
 
-            tft.text((2, 70), "pH raw: " + str(ph_raw), TFT.BLACK, sysfont, 1.1, nowrap=False)
-            tft.text((2, 80), "pH : " + str(ph), TFT.BLACK, sysfont, 1.1, nowrap=False)
+def init_display():
+    global _TFT
+    wifi_ssid = WIFI_SSID
+    if len(wifi_ssid) >= 14:
+        wifi_ssid = WIFI_SSID[0:14]
+    _TFT.fill(TFT.BLACK)
+    _TFT.fillrect((0, 0), (128, 50), TFT.WHITE)
+    _TFT.text((2, 2), "Wifi:" + wifi_ssid, TFT.BLACK, sysfont, 1.1, nowrap=False)
+    _TFT.text((2, 10), "Node:" + NODE_TYPE, TFT.BLACK, sysfont, 1.1, nowrap=False)
 
-            tft.text((2, 90), "EC raw: " + str(ec_raw), TFT.BLACK, sysfont, 1.1, nowrap=False)
-            tft.text((2, 100), "EC (mS/m) :  " + str(ec), TFT.BLACK, sysfont, 1.1, nowrap=False)
 
-            tft.text((2, 110), "ORP raw: " + str(orp_raw), TFT.BLACK, sysfont, 1.1, nowrap=False)
-            tft.text((2, 120), "ORP (mV): " + str(orp), TFT.BLACK, sysfont, 1.1, nowrap=False)
+def update_display():
+    global _TFT, flow_dict
+    while True:
+        # Flush dynamic part of screen
+        _TFT.fillrect((0, 50), (128, 160), TFT.GRAY)
+        if flow_dict['current']['water_valve_signal']:
+            _TFT.fillrect((110, 50), (20, 10), TFT.GREEN)
+            _TFT.text((2, 50), "Water valve on", TFT.BLACK, sysfont, 1.1, nowrap=False)
+        else:
+            _TFT.fillrect((110, 50), (20, 10), TFT.RED)
+            _TFT.text((2, 50), "Water valve off", TFT.BLACK, sysfont, 1.1, nowrap=False)
 
-            tft.text((2, 130), "Power: " + str(pk.power), TFT.BLACK, sysfont, 1.1, nowrap=False)
-            # +----------------------------------------------------+
-            # | Activate / Deactivate relay                        |
-            # +----------------------------------------------------+
-            if not last_power:
-                last_power = pk.power
-                PUMP_RELAY.value(pk.power)
-            else:
-                if pk.power != last_power:
-                    PUMP_RELAY.value(pk.power)
-            last_power = pk.power
+        _TFT.text((2, 60), "Raw ADC: " + str(flow_dict['sensors']['soil_moisture_raw_adc']),
+                  TFT.BLACK, sysfont, 1.1, nowrap=False)
+        _TFT.text((2, 70), "Soil Moisture: " + str(flow_dict['sensors']['soil_moisture']),
+                  TFT.BLACK, sysfont, 1.1, nowrap=False)
 
-            gc.collect()
-            time.sleep(0.5)
-        except Exception as ex:
-            PUMP_RELAY.value(0)
-            last_power = 0
-            tft.text((2, 60), "ERROR ", TFT.BLACK, sysfont, 2, nowrap=False)
-            tft.text(
-                (2, 80),
-                str(ex.__class__.__name__) + ":" + str(ex),
-                TFT.BLACK,
-                sysfont,
-                1.2,
-                nowrap=False
-            )
-            gc.collect()
-            time.sleep(2)
+        _TFT.text((2, 70), "Soil Moisture: " + str(flow_dict['sensors']['soil_moisture']),
+                  TFT.BLACK, sysfont, 1.1, nowrap=False)
+        if flow_dict['soft_fuse']:
+            _TFT.text((2, 80), "Soft fuse !", TFT.RED, sysfont, 1.1, nowrap=False)
+
+        _TFT.text((2, 150), __water_firmware_version__,
+                  TFT.BLACK, sysfont, 1.1, nowrap=False)
+        gc.collect()
+
+
+def soft_fuse():
+    global flow_dict
+    while True:
+        if (time.time() - flow_dict['updated_at']) > 1:
+            water_pump_relay.value(False)
+            nutrient_pump_relay.value(False)
+            ph_downer_pump_relay.value(False)
+            probe_cleaning_relay.value(False)
+            flow_dict['current']['water_pump_signal'] = False
+            flow_dict['current']['nutrient_pump_signal'] = False
+            flow_dict['current']['ph_downer_pump_signal'] = False
+            flow_dict['current']['probe_cleaning_signal'] = False
+            flow_dict['soft_fuse'] = True
+        else:
+            flow_dict['soft_fuse'] = False
+
+
+# =================================
+# Subscription to controller
+# =================================
+_thread.start_new_thread(subscribe_controller, ())
+
+# =================================
+# Subscription to controller
+# =================================
+_thread.start_new_thread(publish_sensors, ())
+
+# =================================
+# Update TFT screen
+# =================================
+_thread.start_new_thread(update_display, ())
+
+# =================================
+# Soft fuse loop
+# =================================
+_thread.start_new_thread(soft_fuse, ())
